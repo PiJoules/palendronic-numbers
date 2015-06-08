228911c81b1454292a6b49de93de50dcabf45777
Leo is awesome <lchan1994@yahoo.com>
2015-06-08 14:16:57 -0400

    init commit

---
+++ PalindromicNumberChecker.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PalindromicNumberChecker {
+	public static void main(String[] args){
+		/* Read from stdin */
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNextLine()){
+			
+		}
+	}
+}
