d25ad5b981b71790603a0213d95b8ec80d30b46c
Leo is awesome <lchan1994@yahoo.com>
2015-06-08 17:08:45 -0400

    works

--- PalindromicNumberChecker.java
+++ PalindromicNumberChecker.java
@@ -1,11 +1,44 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class PalindromicNumberChecker {
-	public static void main(String[] args){
-		/* Read from stdin */
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNextLine()){
-			
-		}
-	}
+	private BigInteger num;
+
+    /**
+     * Retrieve the number of steps required to make a palendrom of the number
+     * @param  n string
+     * @return   BigInt
+     */
+    public long getStepCount(String n){
+    	BigInteger num = new BigInteger(n);
+    	BigInteger revNum = reverseNum(num);
+    	long steps = 0;
+
+    	while (!num.equals(revNum)){
+    		num = num.add(revNum);
+    		revNum = reverseNum(num);
+    		steps++;
+    	}
+
+    	this.num = num;
+    	return steps;
+    }
+
+    /**
+     * Get the flipped value of the number
+     * @param  num BigInt
+     * @return     BigInt
+     */
+    private static BigInteger reverseNum(BigInteger num) {
+        String reverseStr = new StringBuilder(num.toString()).reverse().toString();
+        return new BigInteger(reverseStr);
+    }
+
+    /**
+     * Getter for the final palindrome number
+     * @return NigInt
+     */
+    public BigInteger getPalindromeNum(){
+    	return this.num;
+    }
 }
